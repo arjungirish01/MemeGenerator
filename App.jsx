11024534446ed578690ce3df0cbcb92b97328521
Arjun Girish <128234597+arjungirish01@users.noreply.github.com>
2023-09-03 20:38:13 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Generate from "./Generate";
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Generate />
+    </div>
+  );
+}
+
+export default App;
